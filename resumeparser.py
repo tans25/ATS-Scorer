1b62a83a5c09d5fecef22700fb9398c463bce29f
tans25 <tansjoshi25@gmail.com>
2026-03-16 23:28:29 -0400

    solved issue with resume parser file name

---
+++ resumeparser.py
@@ -0,0 +1,36 @@
+import pdfplumber 
+import os 
+
+file_path = '/Users/tanayapravinjoshi/Downloads/TanayaJoshiResume_SWE_intern.pdf'
+
+class ResumeParser:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        
+    def resume_text_extractor(self):
+        result = {}
+        try:
+            text = ''
+            with pdfplumber.open(self.file_path) as pdf:
+                for page in pdf.pages:
+                    page_text = page.extract_text()
+                    if page_text:
+                        text += page_text + "\n"
+            result = {"success":True, "text": text}
+        except Exception as e:
+            result = {"success":False, "message":"Something went wrong"}
+        return result 
+
+    def parse_resume(self):
+        result = {}
+        try: 
+            if not os.path.exists(self.file_path):
+                return {"result":True, "message":"File not found"}
+            ext = os.path.splitext(self.file_path)[-1].lower()
+            if ext == ".pdf":
+                result = self.resume_text_extractor(self.file_path)
+            else:
+                return {"result":True, "message":"File type not supported"}
+        except Exception as e:
+            result = {"result":False, "message":"Something went wrong"}
+        return result 
